3a44dede8ad3ad131c9063ea8b4ff9153a8aa115
ezzzinne <cynthia.nwani017@gmail.com>
2025-03-16 23:21:32 +0100

    added changes

--- userDashboard/src/main.tsx
+++ userDashboard/src/main.tsx
@@ -3,15 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from './context/AuthContext.tsx'
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
 
